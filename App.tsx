a057063a4013540b9461f79bf0ef26d446968f00
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-07-17 14:17:29 +0900

    Add the same options as ActivityIndicator (#2)

--- App.tsx
+++ App.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { StyleSheet, useWindowDimensions, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, useWindowDimensions, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Spinner } from './Spinner';
+import { useState } from 'react';
 
 export default function App() {
+  const [animating, setAnimating] = useState(true);
   const windowWidth = useWindowDimensions().width;
   const spinnerWidth = windowWidth * 0.25;
   const spinnerBorderWidth = windowWidth * 0.025;
 
   return (
     <View style={styles.container}>
-      <Spinner
-        duration={1500}
-        width={spinnerBorderWidth}
-        color="#f33"
-        backgroundColor="#ddd"
-        style={{ width: spinnerWidth, height: spinnerWidth }}
-      />
+      <View style={{ alignItems: 'center' }}>
+        <Spinner
+          animating={animating}
+          duration={1500}
+          width={spinnerBorderWidth}
+          color="#f33"
+          backgroundColor="#ddd"
+          style={{ width: spinnerWidth, height: spinnerWidth }}
+        />
+        <TouchableOpacity
+          style={{ borderRadius: 10, backgroundColor: '#ccc', padding: 10, marginTop: 10 }}
+          onPress={() => setAnimating(!animating)}>
+          <Text>{animating ? 'Stop animation' : 'Start animation'}</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
--- Spinner.tsx
+++ Spinner.tsx
@@ -1,38 +1,56 @@
 import React, { useEffect, useRef } from 'react';
-import { StyleProp, View, ViewStyle, Animated, Easing } from 'react-native';
+import { useMemo } from 'react';
+import { StyleSheet, StyleProp, View, ViewStyle, Animated, Easing, ColorValue } from 'react-native';
 
 const CIRCLE_RADIUS = 9999;
 
 export const Spinner: React.VFC<{
-  duration?: number | undefined | null;
-  width?: number | undefined | null;
-  color?: string | undefined | null;
-  backgroundColor?: string | undefined | null;
+  animating?: boolean;
+  hidesWhenStopped?: boolean;
+  duration?: number;
+  width?: number;
+  color?: ColorValue;
+  backgroundColor?: ColorValue;
   style?: StyleProp<ViewStyle>;
-}> = ({
-  duration: durationParam,
-  width: widthParam,
-  color: colorParam,
-  backgroundColor: backgroundColorParam,
-  style,
-}) => {
+}> = (props) => {
+  // Props
+  const animating = props.animating ?? true;
+  const hidesWhenStopped = props.hidesWhenStopped ?? true;
+  const duration = props.duration ?? 2000;
+  const width = props.width ?? 5;
+  const color = props.color ?? 'white';
+  const backgroundColor = props.backgroundColor ?? 'rgba(255, 255, 255, 0.6)';
+  const style = StyleSheet.flatten(props.style);
+
+  // Only size properties pulled from `style`
+  const spinnerStyle: ViewStyle = {
+    width: style.width,
+    height: style.height,
+  };
+
   const spinValue = useRef(new Animated.Value(0)).current;
 
-  const duration = durationParam ?? 2000;
-  const width = widthParam ?? 5;
-  const color = colorParam ?? 'white';
-  const backgroundColor = backgroundColorParam ?? 'rgba(255, 255, 255, 0.6)';
+  const loop = useMemo(
+    () =>
+      Animated.loop(
+        Animated.timing(spinValue, {
+          toValue: 360,
+          duration,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        })
+      ),
+    [spinValue, duration]
+  );
 
   useEffect(() => {
-    Animated.loop(
-      Animated.timing(spinValue, {
-        toValue: 360,
-        duration,
-        easing: Easing.linear,
-        useNativeDriver: true,
-      })
-    ).start();
-  }, [spinValue, duration]);
+    if (animating) {
+      spinValue.setValue(0);
+      loop.start();
+    } else {
+      loop.stop();
+    }
+  }, [loop, animating]);
 
   const rotateValue = spinValue.interpolate({
     inputRange: [0, 360],
@@ -44,31 +62,35 @@ export const Spinner: React.VFC<{
   //
   // https://github.com/facebook/react-native/issues/9262
   return (
-    <View>
-      <View
-        style={[
-          {
-            position: 'absolute',
-            borderRadius: CIRCLE_RADIUS,
-            borderColor: backgroundColor,
-            borderWidth: width,
-          },
-          style,
-        ]}></View>
-      <Animated.View
-        style={[
-          {
-            borderRadius: CIRCLE_RADIUS,
-            borderColor: backgroundColor,
-            borderTopWidth: width,
-            borderTopColor: color,
-            borderRightWidth: width,
-            borderLeftWidth: width,
-            transform: [{ rotate: rotateValue }],
-          },
-          style,
-        ]}
-      />
+    <View style={style}>
+      {animating || !hidesWhenStopped ? (
+        <React.Fragment>
+          <View
+            style={[
+              {
+                position: 'absolute',
+                borderRadius: CIRCLE_RADIUS,
+                borderColor: backgroundColor,
+                borderWidth: width,
+              },
+              spinnerStyle,
+            ]}></View>
+          <Animated.View
+            style={[
+              {
+                borderRadius: CIRCLE_RADIUS,
+                borderColor: backgroundColor,
+                borderTopColor: color,
+                borderTopWidth: width,
+                borderRightWidth: width,
+                borderLeftWidth: width,
+                transform: [{ rotate: rotateValue }],
+              },
+              spinnerStyle,
+            ]}
+          />
+        </React.Fragment>
+      ) : null}
     </View>
   );
 };
